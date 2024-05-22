21253b78b4268a168326c49432e29088f4687021
MD AHSANUL HOQUE ABIR <113261318+ahsanulhoqueabir@users.noreply.github.com>
2024-05-22 21:57:55 +0600

    "Replaced empty file with user route implementation"

--- route/user.js
+++ route/user.js
@@ -1,1 +1,29 @@
-usudgshf
+const express = require("express");
+const {
+  createUser,
+  getUsers,
+  addUser,
+  deleteUser,
+  updateUser,
+  getInstructor,
+  queryUser,
+  addSelectedClass,
+  removeSelectedClass,
+  updateRole,
+} = require("../controller/user.js");
+const { verifyAdmin, verifyJWT } = require("../middleware/middleware.js");
+const jwt = require("jsonwebtoken");
+
+const router = express.Router();
+
+router.post("/create", createUser);
+router.get("/alluser", getUsers);
+router.get("/query", queryUser); // add verifyJWT
+router.put("/add", addUser);
+router.delete("/delete/:id", deleteUser);
+router.put("/update", verifyJWT, updateUser);
+router.get("/instructor", getInstructor);
+router.put("/updaterole", updateRole);
+router.put("/addSelectedClass", addSelectedClass); // add verifyJWT
+router.delete("/RemoveClass", removeSelectedClass); // add verifyJWT
+module.exports = router;
